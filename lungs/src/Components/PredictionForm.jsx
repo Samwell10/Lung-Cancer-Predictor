341c6b8bb91a41f946e5fd46f857ae61276e0991
unknown <aleriwaprecious70@gmail.com>
2025-11-16 23:35:08 -0500

    Live link added to client

--- lungs/src/Components/PredictionForm.jsx
+++ lungs/src/Components/PredictionForm.jsx
@@ -94,7 +94,7 @@ export default function LungCancerPredictor() {
     setPrediction(null);
     try{
 
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://lung-cancer-predictor-uck2.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
